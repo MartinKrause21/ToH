e1a50b0054b4b04a1b71ea0e5a219b6af620960b
MartinKrause21 <martin.21.krause@gmail.com>
2021-10-17 11:14:27 +0200

    Add tiems css style

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -11,6 +11,11 @@ export class ItemsComponent implements OnInit {
 
   items = ITEMS;
 
+  selectedItem?: Item;
+  onSelect(item: Item): void {
+  this.selectedItem = item;
+}
+
   constructor() { }
   ngOnInit(): void { }
 }
