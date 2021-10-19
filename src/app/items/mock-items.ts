f11e92097bf2be389adb3f4a155bf32c5a2925af
Matolim <martin.21.krause@gmail.com>
2021-10-19 10:05:04 +0200

    Add connection with item and hero

--- src/app/items/mock-items.ts
+++
@@ -1,6 +0,0 @@
-import { Item } from '../item';
-
-export const ITEMS: Item[] = [
-  { id: 11, name: 'AXE', price: 20 },
-  { id: 12, name: 'Sword', price: 15 },
-];
---
+++ src/app/mock-items.ts
@@ -0,0 +1,12 @@
+import { Item } from './item';
+
+
+export const Items: Item[] = [
+  { id: 11, name: 'AXE', price: 20 }, //0
+  { id: 12, name: 'Sword', price: 15 }, //1
+  { id: 13, name: 'Machette', price: 36 }, //2
+  { id: 14, name: 'Bat', price: 100 }, //3
+  { id: 15, name: 'Knife', price: 80 }, //4
+  { id: 16, name: 'Katana', price: 200 }, //5
+  { id: 17, name: 'Gun', price: 500 }, //6
+];
